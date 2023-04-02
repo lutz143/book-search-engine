27f3d23d35310ce8285a27258144c84f7721f993
Josh <lutz_143@yahoo.com>
2023-04-02 15:10:52 -0400

    updates to LoginForm to handle mutations for logging in

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -13,6 +13,8 @@ const SignupForm = () => {
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
 
+  const [addUser, {error }] = useMutation(ADD_USER);
+
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
@@ -29,15 +31,14 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      const { data } = await addUser({ variables: { ...userFormData }});
 
       if (!response.ok) {
         throw new Error('something went wrong!');
       }
 
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      Auth.login(data.addUser.token);
+      
     } catch (err) {
       console.error(err);
       setShowAlert(true);
